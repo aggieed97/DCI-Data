4a9d935a71f61e633a3ab494f93d0fb7e37afc6b
Ed Salinas <aggieed97@gmail.com>
2023-06-19 09:41:34 -0500

    Add sh update file

---
+++ dci_data_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/main.py
+
+~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/small-recap-scrape.py
+
+~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/large-recap-scrape.py
+
