893ef6d6c1b5679f89d77e7ae0e9a1dc414306fa
Ed Salinas <aggieed97@gmail.com>
2023-06-29 09:04:00 -0500

    Update bash script and rank_by_max_score

--- dci_data_update.sh
+++ dci_data_update.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/main.py
+#~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/main.py
 
-~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/small-recap-scrape.py
+#~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/small-recap-scrape.py
 
-~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/large-recap-scrape.py
+#~/Python-Projects/DCI-Data/venv/bin/python /home/aggieed97/Python-Projects/DCI-Data/large-recap-scrape.py
+
+cd ~/R-Projects/DCI-R-Projects/
+
+/usr/lib/R/bin/Rscript -e 'renv::activate()'
+
+/usr/lib/R/bin/Rscript ~/R-Projects/DCI-R-Projects/DCI-Max-Score-Table.R
 
